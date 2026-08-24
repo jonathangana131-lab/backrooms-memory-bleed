a14436a0a096059ed1a79c7d4df3940a95f554c1
Joey <joey@Danys-iMac.local>
2026-08-24 07:58:53 -0700

    feat: test/ui-meta-v1-entry.ts (F47 daily rite / F49 access pack / F93 diegetic menus / F97 form toasts)

---
+++ test/ui-meta-v1-entry.ts
@@ -0,0 +1,5 @@
+export * from '../src/ui/dailyrite';
+export * from '../src/ui/accessibilitypack';
+export * from '../src/ui/formtoasts';
+export * from '../src/ui/diegmenus';
+export { seedFromString } from '../src/core/rng';
